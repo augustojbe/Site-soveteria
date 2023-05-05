6c0a2eabf6c1e55b33be8a0db4a3400e9b6b5b09
Augusto <augustojbe@gmail.com>
2023-05-04 21:37:50 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Rotas from '../src/rotas';
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+           
+      <Rotas/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Topo/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './style.css'
+import { Link } from "react-router-dom";
+
+
+export default function Topo (){
+
+    return (
+        <header>
+            <div className="limitar-secao">
+                <img src="assets/logo.png" alt="logomarca"/>
+                <nav>
+                    <Link className="link-topo" to="/">Home</Link>
+                    <Link className="link-topo" to="/sabores">Sabores</Link>
+                    <Link className="link-topo" to="/sobre">Sobre</Link>
+                </nav>
+                
+            </div>
+        </header>
+    )
+
+
+};
